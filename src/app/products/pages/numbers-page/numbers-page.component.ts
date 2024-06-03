b08d98b1e3cc80d75c8ec7ebb5535640a686ec23
kathm <kathmontalvo123@gmail.com>
2024-06-03 18:56:17 -0500

    add uncommon pipes

--- src/app/products/pages/numbers-page/numbers-page.component.ts
+++ src/app/products/pages/numbers-page/numbers-page.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class NumbersPageComponent {
 
+  public totalSells: number = 2422323.243;
+  public percent: number = 0.4856;
+
 }
--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval, tap } from 'rxjs';
 
 @Component({
   selector: 'app-uncommon-page',
@@ -7,4 +8,64 @@ import { Component } from '@angular/core';
 })
 export class UncommonPageComponent {
 
+  //i18nSelect
+  public name: string = 'kath';
+  public gender: 'male'|'female'|'nb' = 'female';
+  public invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla',
+    nb: 'invitarle'
+  }
+
+  changeGender(gender: string):void {
+    switch (gender) {
+      case 'female':
+        this.name = 'kath';
+        this.gender = 'female';
+        break;
+      case 'male':
+        this.name = 'abhay';
+        this.gender = 'male';
+        break;
+      case 'nb':
+        this.name = 'burako';
+        this.gender = 'nb';
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  //i18nPlural
+  public clients: string[] = ['kath', 'abhay', 'bu', 'fio', 'rodrigo', 'thais', 'dani'];
+  public clientsMap= {
+    '=0': 'no tenemos ningún cliente',
+    '=1': 'tenemos 1 cliente',
+    'other': 'tenemos # clientes'
+  }
+  deleteClient():void {
+    this.clients.pop();
+  }
+
+  //KeyValue Pipe
+
+  public person = {
+    name: 'kath',
+    age: 26,
+    address: 'chorrillos'
+  }
+
+  // async pipe
+
+  public myObservableTimer= interval(2000).pipe(
+    tap( value => console.log('tap:', value)),
+  )
+
+  public promiseValue: Promise<string> = new Promise( (resolve, rej) => {
+    setTimeout(() => {
+      resolve('Tenemos data en la promesa.');
+      console.log('Tenemos data en la promesa.')
+    }, 3500)
+  })
 }
